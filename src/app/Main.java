4e4bbbd5fc1ef95da769762f0dc2463ec8b211b2
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-10-05 16:08:06 +0300

    Fix issue in Main.java: Add customer init data, fix print information

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,22 @@
 package app;
 
 public class Main {
+
+    public static void main(String[] args) {
+        Customer customer = getCustomer(getData());
+        String output = String.format("Customer: %s, phone: %s", customer.getName(), customer.getPhone());
+        getOutput(output);
+    }
+
+    public static String[] getData() {
+        return new String[]{"Vasyl", "5551238596"};
+    }
+
+    public static Customer getCustomer(String[] data) {
+        return new Customer(data[0], data[1]);
+    }
+
+    public static void getOutput(String output) {
+        System.out.println(output);
+    }
 }
