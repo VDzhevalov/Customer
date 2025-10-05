1870184ad50e0b32c07ed5e3856f96dc3d5bb1d3
Volodymyr Dzhevalov <vladtestac@gmail.com>
2025-10-05 16:52:03 +0300

    Import CustomerUtil to Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,23 +1,13 @@
 package app;
 
+import static app.utils.CustomerUtil.*;
+
 public class Main {
 
     public static void main(String[] args) {
         Customer customer = getCustomer(getData());
         String output = getFormattedCustomerInfo(customer);
         getOutput(output);
-    }
-
-    public static String[] getData() {
-        return new String[]{"Vasyl", "5551238596"};
-    }
-
-    public static Customer getCustomer(String[] data) {
-        return new Customer(data[0], data[1]);
-    }
-
-    public static String getFormattedCustomerInfo(Customer customer) {
-        return String.format("Customer: %s, phone: %s", customer.getName(), customer.getPhone());
     }
 
     public static void getOutput(String output) {
